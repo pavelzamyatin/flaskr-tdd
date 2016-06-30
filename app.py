e9bdd63a1e54876e78001540560d692e1cf25358
Corw <pzamyatin@gmail.com>
2016-06-30 15:21:35 +1200

    jQuery, delete func

--- app.py
+++ app.py
@@ -89,6 +89,20 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
 
+@app.route('/delete/<post_id>', methods=['GET'])
+def delete_entry(post_id):
+    '''Delete post from database'''
+    result = {'status': 0, 'message': 'Error'}
+    try:
+        db = get_db()
+        db.execute('delete from entries where id=' + post_id)
+        db.commit()
+        result = {'status': 1, 'message': "Post Deleted"}
+    except Exception as e:
+        result = {'status': 0, 'message': repr(e)}
+
+    return jsonify(result)
+
 if __name__ == '__main__':
 	#init_db()
 	app.run()
