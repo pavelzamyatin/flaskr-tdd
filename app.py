afe46220ed360a75d65480716b21a9c4daae32d2
Corw <pzamyatin@gmail.com>
2016-06-30 13:30:34 +1200

    Basic files added

---
+++ app.py
@@ -0,0 +1,49 @@
+# imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify
+
+# configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+# create and initialize app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# connect to database
+def connect_db():
+    """Connects to the database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+# create the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+# open database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+# close database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+if __name__ == '__main__':
+	init_db()
+	app.run()
